24387bebd5f1d4546b2fecc01a30fbdf0703a956
Emilio Basenghi <basenghitwitter@gmail.com>
2025-11-05 22:03:23 +0100

    login very basic

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import streamlit as st
-from streamlit_echarts import st_echarts
-import pandas as pd
-import plotly.express as px
-import numpy as np
+st.title("Pizza Radar 🍕")
 
-st.set_page_config(layout="wide")
-st.title("La Mia Pizzeria")
-
-st.expander("💡 Analisi AI", expanded=True)
-
-tab1, tab2, tab3 = st.tabs([" 💬 Cosa dicono di noi ", " 🔍 Dentro la nostra Offerta ", " 🍕 Testa a Testa "])
-
+if not st.user.is_logged_in:
+    if st.button("Authenticate"):
+        st.login("auth0")
+else:
+    if st.button("Logout"):
+        st.logout()
