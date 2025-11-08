2bc593cb5e9b6ed563ad2ac14b94bd175b7f187d
Emilio Basenghi <basenghitwitter@gmail.com>
2025-11-08 15:18:56 +0100

    pagination

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import streamlit as st
-st.title("Pizza Radar 🍕")
+
+# Page definitions
+p1 = st.Page("pages/panoramica/competition.py", title="Pizzerie in zona", icon=":material/leaderboard:", url_path="pizzerie-in-zona")
+p2 = st.Page("pages/analisi recensioni/coach.py", title="Coach", icon=":material/school:", url_path="coach")
+p3 = st.Page("pages/metrics/your_metrics.py", title="Le tue metriche", icon=":material/search_insights:", url_path="le-tue-metriche")
+p4 = st.Page("pages/metrics/compare.py", title="Confrontati con altre pizzerie", icon=":material/compare_arrows:", url_path="confrontati-con-altre-pizzerie")
+p5 = st.Page("pages/altro/who_we_are.py", title="Chi siamo", icon=":material/people_outline:", url_path="chi-siamo")
+p6 = st.Page("pages/altro/contact_us.py", title="Contattaci", icon=":material/mail:", url_path="contattaci")
+p7 = st.Page("pages/altro/account.py", title="Il tuo account", icon=":material/account_circle:", url_path="il-tuo-account")
+
 
 if not st.user.is_logged_in:
     if st.button("Authenticate"):
         st.login("auth0")
 else:
-    if st.button("Logout"):
-        st.logout()
+    pg = st.navigation({
+        "Panoramica": [p1],
+        "Analisi recensioni": [p2],
+        "Metriche": [p3, p4],
+        "Altro": [p7, p5, p6]
+        }, position="top")
+    pg.run()
+
+st.json(st.user.to_dict())
---
+++ pages/altro/account.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.write("Account Page")
+
+if st.button("Logout"):
+    st.logout()
